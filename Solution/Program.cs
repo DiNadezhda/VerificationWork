843428f5064029306d30037240b4927c4ce4f473
Nadezhda <402630@bk.ru>
2023-02-16 19:07:13 +0700

    Программа написана

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -6,12 +6,47 @@
 ["1234", "1567", "-2", "computer science"]->["-2"]
 ["Russia", "Denmark", "Kazan"]->[] */
 
+string[] array = ArrayInput(3);
+Console.WriteLine();
+PrintArray(array);
 
+Console.Write(" -> ");
+string[] array3 = СreatingNewArray(array, 3);
+PrintArray(array3);
 
 //Метод для ввода массива с клавиатуры
-
+string[] ArrayInput(int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите {i + 1} слово: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
 
 //Метод, создающий массив из элементов определенной длины
-
+string[] СreatingNewArray(string[] arr, int letters)
+{
+    string[] arrLimit = new string[arr.Length];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= letters) arrLimit[j] = arr[i];
+        j++;
+    }
+    return arrLimit;
+}
 
 //Метод печати массива
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1 && arr[i] != null) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
